58a83fac230683293913a8a78ea529aaed632985
rachelrajan_exp <rachel.rajan@experionglobal.com>
2024-08-26 18:18:44 +0530

    Partitions table added

---
+++ XperioDeskBE/app/Models/PartitionTable.php
@@ -0,0 +1,33 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\SoftDeletes;
+
+class PartitionTable extends Model
+{
+    use HasFactory, SoftDeletes;
+
+    protected $table = 'partitions_tables'; // Specify the table name
+
+    protected $fillable = [
+        'layout_entity_id',
+        'x-position',
+        'height',
+        'width',
+        'y-position',
+    ];
+
+    protected $dates = ['deleted_at']; // For soft deletes
+
+    /**
+     * Get the layout entity associated with this partition.
+     */
+    public function layoutEntity()
+    {
+        return $this->belongsTo(LayoutEntity::class, 'layout_entity_id');
+    }
+
+}
